6e2ad2ad27fbab1690ae73b3b13a0e8080bb1025
arbaaz <abdularbaaz@gmail.com>
2019-08-16 18:02:02 +0530

    Initial commit

---
+++ src/main.elm
@@ -0,0 +1,57 @@
+module Main exposing (main)
+
+import Element
+import Svg
+import Svg.Attributes
+
+
+main =
+    [ Svg.circle
+        [ Svg.Attributes.r "10"
+        , Svg.Attributes.cx "0"
+        , Svg.Attributes.cy "0"
+        , Svg.Attributes.transform "translate(80)"
+        , Svg.Attributes.fill "skyblue"
+        ]
+        []
+    , Svg.circle
+        [ Svg.Attributes.r "10"
+        , Svg.Attributes.cx "40"
+        , Svg.Attributes.cy "0"
+        , Svg.Attributes.fill "orange"
+        , Svg.Attributes.transform "rotate(72) translate(80)"
+        ]
+        []
+    , Svg.circle
+        [ Svg.Attributes.r "10"
+        , Svg.Attributes.cx "80"
+        , Svg.Attributes.cy "0"
+        , Svg.Attributes.fill "red"
+        , Svg.Attributes.transform "rotate(144) translate(80)"
+        ]
+        []
+    , Svg.circle
+        [ Svg.Attributes.r "10"
+        , Svg.Attributes.cx "120"
+        , Svg.Attributes.cy "0"
+        , Svg.Attributes.fill "lime"
+        , Svg.Attributes.transform "rotate(216) translate(80)"
+        ]
+        []
+    , Svg.circle
+        [ Svg.Attributes.r "10"
+        , Svg.Attributes.cx "160"
+        , Svg.Attributes.cy "0"
+        , Svg.Attributes.fill "maroon"
+        , Svg.Attributes.transform "rotate(288) translate(80)"
+        ]
+        []
+    ]
+        |> Svg.svg
+            [ Svg.Attributes.style "background: pink"
+            , Svg.Attributes.height "100%"
+            , Svg.Attributes.width "100%"
+            , Svg.Attributes.viewBox "-300 -300 600 600"
+            ]
+        |> Element.html
+        |> Element.layout [ Element.width Element.fill, Element.height Element.fill ]
